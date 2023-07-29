2ef378aa11fc78cf0c40da2d8e5fd0c26b3251e2
Yeonwu Oh <yeonwu319@gmail.com>
2023-07-29 13:50:31 +0900

    [#4] Refactor: uno-render의 내용을 app.cpp와 common.cpp로 분리
    - 함수, 전역변수, 클래스는 app.cpp
    - 전체 프로젝트에서 공유해서 쓰는 상수는 common.cpp

---
+++ common.hpp
@@ -0,0 +1,12 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#define byte uint8_t
+#define bitRead(value, bit) (((value) >> (bit)) & 0x01)
+
+#define MAP_WIDTH 8
+#define MAP_HEIGHT 6
+
+#define TILE_SIZE 16
+
+#endif
