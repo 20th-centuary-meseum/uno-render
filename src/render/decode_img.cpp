8a5e152c5b553fdfddeb7d7db17442ac59cc1771
Yeonwu Oh <yeonwu319@gmail.com>
2023-08-09 22:34:47 +0900

    [#10] Fix: 세미콜론 빠진거 추가, images 이중포인터 배열로 변경

--- src/render/decode_img.cpp
+++ src/render/decode_img.cpp
@@ -1,43 +1,59 @@
 #include <TVout.h>
-#include "../../common.hpp"
 #include "decode_img.hpp"
+#include "../../common.hpp"
 
 TVout TV;
 
 byte sprite[32] = {
-  0b00000000, 0b00000000,
-  0b01111111, 0b11111110,
-  0b01000000, 0b00000010,
-  0b01000000, 0b00000010,
-  0b01000000, 0b00000010,
-  0b01000000, 0b00000010,
-  0b01000000, 0b00000010,
-  0b01000000, 0b00000010,
-  0b01000000, 0b00000010,
-  0b01000000, 0b00000010,
-  0b01000000, 0b00000010,
-  0b01000000, 0b00000010,
-  0b01000000, 0b00000010,
-  0b01000000, 0b00000010,
-  0b01111111, 0b11111110,
-  0b00000000, 0b00000000,
-}
-byte **images = {sprite};
+    0b00000000,
+    0b00000000,
+    0b01111111,
+    0b11111110,
+    0b01000000,
+    0b00000010,
+    0b01000000,
+    0b00000010,
+    0b01000000,
+    0b00000010,
+    0b01000000,
+    0b00000010,
+    0b01000000,
+    0b00000010,
+    0b01000000,
+    0b00000010,
+    0b01000000,
+    0b00000010,
+    0b01000000,
+    0b00000010,
+    0b01000000,
+    0b00000010,
+    0b01000000,
+    0b00000010,
+    0b01000000,
+    0b00000010,
+    0b01000000,
+    0b00000010,
+    0b01111111,
+    0b11111110,
+    0b00000000,
+    0b00000000,
+};
+byte *images[] = {sprite};
 
 void DecodeImg(byte image_byte, byte x_pos, byte y_pos)
 {
-	byte image_id = image_byte *= 2;
-	byte image_rot = bitRead(image_byte, 0) + 2 * bitRead(image_byte, 0);
+  byte image_id = image_byte *= 2;
+  byte image_rot = bitRead(image_byte, 0) + 2 * bitRead(image_byte, 0);
 
-  byte *image_binary = *(images+ image_id);
+  byte *image_binary = *(images + image_id);
 
-  int tmp_y = y_pos*TILE_SIZE*MAP_WIDTH/8;
+  int tmp_y = y_pos * TILE_SIZE * MAP_WIDTH / 8;
 
-  for(int i = 0; i<TILE_SIZE; i++){
-    TV.screen[tmp_y + x_pos/8 + i*TILE_SIZE*MAP_WIDTH/8] = *(image_binary + i*2);
-    TV.screen[tmp_y + x_pos/8 + i*TILE_SIZE*MAP_WIDTH/8 + 1] = *(image_binary + i*2 + 1);
+  for (int i = 0; i < TILE_SIZE; i++)
+  {
+    TV.screen[tmp_y + x_pos / 8 + i * TILE_SIZE * MAP_WIDTH / 8] = *(image_binary + i * 2);
+    TV.screen[tmp_y + x_pos / 8 + i * TILE_SIZE * MAP_WIDTH / 8 + 1] = *(image_binary + i * 2 + 1);
   }
-	
 }
 void beginTV() { TV.begin(_NTSC, 128, 96); }
 
