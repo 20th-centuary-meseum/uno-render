27a56da4c27e85aa6f1e1973fb738ade46bb0b01
Yeonwu Oh <yeonwu319@gmail.com>
2023-11-21 22:29:30 +0900

    press start 깜박거림 구현

--- src/loops/start_page_loop.cpp
+++ src/loops/start_page_loop.cpp
@@ -5,9 +5,21 @@
 
 void start_page_loop()
 {
+	unsigned long last = millis();
+	unsigned long current = millis();
+
+	bool show_text = true;
+
 	while (!CON_START(con1 | con2))
 	{
-		DecodeFull(START_PAGE);
+		unsigned long current = millis();
+		if (last - current > 1000)
+		{
+			show_text = !show_text;
+			last = millis();
+		}
+
+		DecodeFull(show_text ? START_PAGE_A : START_PAGE_B);
 		update_controller();
 	}
 }
